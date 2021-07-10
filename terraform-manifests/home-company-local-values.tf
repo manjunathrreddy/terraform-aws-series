e403a089f1d63521b66ad95f16149c3047133ec1
Manjunath R Reddy <manjunath.ramreddy@gmail.com>
2021-07-10 20:09:17 +0530

    changed manifest files to reflect the use-case

--- terraform-manifests/home-company-local-values.tf
+++ terraform-manifests/home-company-local-values.tf
@@ -3,8 +3,7 @@ locals {
   owners = var.home_company
   environment = var.environment
   name = "${var.home_company}-${var.environment}"
-  #name = "${local.owners}-${local.environment}"
-  common_tags = {
+    common_tags = {
     owners = local.owners
     environment = local.environment
   }
